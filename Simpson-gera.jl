b864077cc351e92e28e07b880713edd85c671017
Jaqueline Ortiz <43250276+jaquelineortiz@users.noreply.github.com>
2019-05-20 18:46:49 -0300

    Create Simpson-gera.jl

---
+++ Simpson-gera.jl
@@ -0,0 +1,30 @@
+function Simpson(f, a, b, c, d, n, m)
+    h = (b - a) / n
+    k(x) = (d(x) - c(x)) / m
+    S = 0.0
+    for i = 0:n
+        if i == 0 || i == n
+            α = 1 * k(a + i * h) / 3
+        elseif mod(i,2) == 0
+            α = 2 * k(a + i * h) / 3
+        else
+            α = 4 * k(a + i * h) / 3
+        end
+        for j = 0:m
+            if j == 0
+                δ = 1
+            elseif j == m
+                δ = 1
+            elseif mod(j,2) == 0
+                δ = 2
+            else
+                δ = 4
+            end
+        xi = a + i * h
+        yj(xi) = c(xi) + j * k(xi)
+        S = S + (α * δ * f(xi, yj))
+        end
+    end
+    I = ((h / 3) * S)
+    return I
+end
