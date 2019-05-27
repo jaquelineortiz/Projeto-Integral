08285b2c58bbfbb9d581992fad04d1e33688fd55
Jaqueline Ortiz <43250276+jaquelineortiz@users.noreply.github.com>
2019-05-27 14:01:52 -0300

    Update funcoes-teste.jl

--- funcoes-teste.jl
+++ funcoes-teste.jl
@@ -1,7 +1,27 @@
-f(x,y) = x - (3 * y^2) a = 0.0; b = 2.0; c = 1.0; d = 2.0; n = m = 2
+Com n = m = 4
+f(x, y) = x * y^2; a = 2.1; b = 2.5; c = 1.2; d = 1.4
 
-f(x,y) = ln(x + 2 * y) a = 1.4; b = 2.0; c = 1.0; d = 1.5; n = 2; m = 4
+f(x, y) = exp(y - x); a = 0; b = 0.5; c = 0; d = 0.5
 
-f(x,y) = y * sin(x * y) a = 1.0; b = 2.0; c = 0.0; d = π; n = ; m = 
+f(x, y) = (x^2 + y^3); a = 2; b = 2.2; c(x) = x; d(x) = 2 * x
+
+f(x, y) = (x^2 + sqrt(y)); a = 1; b = 1.5; c(x) = 0; d(x) = x
+
+Com n = 4, m = 8; n = 8, m = 4; n = m = 6
+f(x, y) = (2 * y * sin(x) + (cos(x))^2); a = 0; b = pi/4; c(x) = sin(x); d(x) = cos(x)
+
+f(x, y) = ln(x * y); a = 0; b = e; c(x) = 1; d(x) = x
+
+f(x, y) = (x^2 + y^3); a = 0; b = 1; c(x) = x; d(x) = 2 * x
+
+f(x, y) = (y^2 + x^3); a = 0; b = 1; c(x) = x; d(x) = 2 * x
+
+f(x, y) = cos(x); a = 0; b = pi; c(x) = 0; d(x) = x
+
+f(x, y) = cos(y); a = 0; b = pi; c(x) = 0; d(x) = x
+
+f(x, y) = 1 / (sqrt(1 - y^2)); a = 0; b = pi/4; c(x); d(x) = sin(x)
+
+f(x, y) = (y * sin(x) + x * cos(y)); a = -pi; b = 3*pi/2; c = 0, d = 2*pi
 
 
