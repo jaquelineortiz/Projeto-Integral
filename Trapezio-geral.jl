82aa27f66fb2d3ac69f65cf9a9af65e0b4be6d05
Jaqueline Ortiz <43250276+jaquelineortiz@users.noreply.github.com>
2019-05-27 13:45:37 -0300

    Update Trapezio-geral.jl

--- Trapezio-geral.jl
+++ Trapezio-geral.jl
@@ -1,1 +1,24 @@
-
+function Trapezio(f, a, b, c, d, m, n)
+    h = (b - a) / n
+    k(x) = (d(x) - c(x)) / m
+    S = 0.0
+    for i = 0:n
+        if i == 0 || i == n
+            α = 1 * k(a + i * h)
+        else
+            α = 2 * k(a + i * h)
+        end
+        for j = 0:n
+            if j == 0 || j == m
+                δ = 1
+            else
+                δ = 2
+            end
+        xi = a + i * h
+        yj = c(xi) + j * k(xi)
+        S = S + (α * δ) * f(xi, yj)
+        end
+    end
+    I = ((h / 3) * S)
+    return I
+end
