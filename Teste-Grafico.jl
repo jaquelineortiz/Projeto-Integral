0a9253d92d309368734ee151a69eaf303bd7ad18
Jaqueline Ortiz <43250276+jaquelineortiz@users.noreply.github.com>
2019-10-18 15:09:10 -0300

    Update Teste-Grafico.jl

--- Teste-Grafico.jl
+++ Teste-Grafico.jl
@@ -1,1 +1,21 @@
+include("Simpson-Adaptativo.jl")
+include("Simpson.jl")
+#include estrutura?
 
+for ϵ = 3.0 .^ (-2:-1:-30)
+    F = função com cache?
+    SA = simpson_adaptivo(f, a, b, c, d, ϵ)   #usocomCache???
+    nf = lenght(F)
+    guarda(NF, ϵ) #função tem guardado?
+end
+scatter(nf, ϵ, xaxis=:log)
+
+#Calculo Simpson Repetido
+for n = 3:2:101
+    for m = 3:2:101
+    SR = Simpson(f, a, b, c, d, n, m)    #usocomCache???
+    erro = abs(SR - I)
+    guarda_erro = #mesma funçao?
+    end
+end
+scatter
