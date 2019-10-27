4092130fd88252401984a38360cb427fb5aebad8
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-10-27 11:09:07 -0300

    Mais correções na imagem

--- Teste-Grafico.jl
+++ Teste-Grafico.jl
@@ -10,31 +10,33 @@ a = c = 0
 b = d = 0.5
 VI = (exp(0.5) - 1) * (1 - exp(-0.5))
 
-NF, Eps, ErroSA, ErroSR = Int[], Float64[], Float64[], Float64[]
+NFSA, NFSR, Eps, ErroSA, ErroSR = Int[], Int[], Float64[], Float64[], Float64[]
 
 for ϵ in 2.0 .^ (-2:-1:-30)
     F = FuncaoComCache(f) # Escolhe alguma f antes
     I = simpson_adaptivo(F, a, b, c, d, ϵ=ϵ)
     push!(Eps, ϵ)
     push!(ErroSA, abs(VI - I))
-    push!(NF, length(F))
+    push!(NFSA, length(F))
 end
 
-# n = m = √max(NF)
-maxn = ceil(Int, sqrt(maximum(NF)))
+# n = m = √max(NFSA)
+maxn = ceil(Int, sqrt(maximum(NFSA)))
 
 for n = 2:2:maxn
     F = FuncaoComCache(f)
     I = Simpson(F, a, b, c, d, n, n)
     push!(ErroSR, abs(VI - I))
+    push!(NFSR, length(F))
 end
-scatter(NF, Eps, c=:green, xaxis=:log2, yaxis=:log, lab="ϵ")
-plot!(NF, Eps, c=:green, xaxis=:log2, yaxis=:log, lab="")
-scatter!(NF, ErroSA, c=:blue, xaxis=:log2, yaxis=:log, lab="Simpson-Adaptivo")
-plot!(NF, ErroSA, c=:blue, xaxis=:log2, yaxis=:log, lab="")
-scatter!(2:2:maxn, ErroSR, c=:red, xaxis=:log2, yaxis=:log, lab="Simpson-Repetido")
-plot!(2:2:maxn, ErroSR, c=:red, xaxis=:log2, yaxis=:log, lab="")
+plot(xaxis=:log2, yaxis=:log)
+scatter!(NFSA, Eps, c=:green, lab="ϵ")
+plot!(NFSA, Eps, c=:green, lab="")
+scatter!(NFSA, ErroSA, c=:blue, lab="Simpson-Adaptivo")
+plot!(NFSA, ErroSA, c=:blue, lab="")
+scatter!(NFSR, ErroSR, c=:red, lab="Simpson-Repetido")
+plot!(NFSR, ErroSR, c=:red, lab="")
 
-ticks = 2 .^ (4:ceil(Int, log2(maximum(NF))))
+ticks = 2 .^ (1:ceil(Int, log2(maximum(NFSA))))
 xticks!(ticks)
 png("IBAGEM")
