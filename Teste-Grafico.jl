cad71aba2b5ccb78f4d67724e371a0a06eb00e94
Jaqueline Ortiz <43250276+jaquelineortiz@users.noreply.github.com>
2019-10-27 11:07:34 -0300

    Update Teste-Grafico.jl

--- Teste-Grafico.jl
+++ Teste-Grafico.jl
@@ -1,3 +1,6 @@
+using Plots
+gr(size=(400,300))
+
 include("Simpson-Adaptativo.jl")
 include("Simpson.jl")
 include("estrutura.jl")
@@ -5,17 +8,14 @@ include("estrutura.jl")
 f = f(x, y) = exp(y - x)
 a = c = 0
 b = d = 0.5
-VI = (exp(0.5) - 1) * (1 - exp(-0.5))
 
-NF, Eps = Int[], Float64[]
+NF = Int[] 
+Eps = Float64[]
 
 for ϵ in 2.0 .^ (-2:-1:-30)
     F = FuncaoComCache(f) # Escolhe alguma f antes
-    I = simpson_adaptivo(f, a, b, c, d, ϵ, VI)
+    I = simpson_adaptivo(F, a, b, c, d, ϵ=ϵ)
     push!(Eps, ϵ)
     push!(NF, length(F))
-
-    #coloco os mesmos passos pra Simpson Repetido aqui?
 end
 scatter(NF, Eps, xaxis=:log, yaxis=:log, lab=:"Simpson-Adaptivo")
-
